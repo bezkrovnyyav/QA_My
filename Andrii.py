495a09a36a3bfc87a90e8b7ab8c1de5f304d965e
bezkrovnyyav <42846260+bezkrovnyyav@users.noreply.github.com>
2018-09-14 11:38:03 +0300

    Delete Andrii.py

--- Andrii.py
+++
@@ -1,9 +0,0 @@
-class Student():
-    def __init__(self, name):
-        self.name = name
-
-        def say_hello(self):
-           print("Hello, " + self.name)
-
-student = Student("Dima")
-student.say_hello()
