102ea133583842ffd2461ef5fc931c96e3b5361e
bezkrovnyyav <42846260+bezkrovnyyav@users.noreply.github.com>
2018-09-14 11:22:21 +0300

    Add files via upload
    Add Andrii

---
+++ Andrii.py
@@ -0,0 +1,9 @@
+class Student():
+    def __init__(self, name):
+        self.name = name
+
+        def say_hello(self):
+           print("Hello, " + self.name)
+
+student = Student("Dima")
+student.say_hello()
