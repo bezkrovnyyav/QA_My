58e129a3264d27ffe1726d1405a4c5dd4092d859
bezkrovnyyav <42846260+bezkrovnyyav@users.noreply.github.com>
2020-08-20 16:11:29 +0300

    Add files via upload

---
+++ Task_Andrei_Bezkrovnyi.py
@@ -0,0 +1,13 @@
+# Create the array
+string = ""
+arr = []
+while True:
+  string = input() # Enter elements of the array
+  if not string:
+    break
+  arr.append(string)
+def rev_mass(arr):
+  result = list(reversed(arr)) # reversing the array
+  return result
+ # displaying the reversed array
+print(rev_mass(arr)) 
