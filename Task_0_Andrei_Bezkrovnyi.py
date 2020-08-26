ff01db64550cc869f4358e43654a1f305f261de0
bezkrovnyyav <42846260+bezkrovnyyav@users.noreply.github.com>
2020-08-26 21:16:52 +0300

    Update Task_0_Andrei_Bezkrovnyi.py

--- Task_0_Andrei_Bezkrovnyi.py
+++ Task_0_Andrei_Bezkrovnyi.py
@@ -1,12 +1,14 @@
 # Create the array
 string = ""
 arr = []
+len_of_arr = 0
 while True:
   string = input() # Enter elements of the array
   if not string:
     break
+  len_of_arr += 1
   arr.append(string)
 def rev_mass(arr):
-  return [arr[i] for i in range(len(arr)-1, -1, -1)] # reversing the array
+  return [arr[i] for i in range(len_of_arr - 1, -1, -1)] # reversing the array
 # displaying the reversed array
 print(rev_mass(arr)) 
